eb6b4606e643c9e54b6806ec8f979840ea3ae5b3
mehmud <timehmud121@gmail.com>
2026-08-15 17:57:41 +0600

    mod sizing

--- src/components/template/ImpactSection.tsx
+++ src/components/template/ImpactSection.tsx
@@ -22,15 +22,6 @@ function ImageWithFallback(props: ComponentProps<typeof Image> & { fallbackLabel
   return <Image {...rest} alt={alt ?? ""} className={className} onError={() => setError(true)} />;
 }
 
-const certifications = [
-  { img: "/icons/edible.png", label: "100% Edible" },
-  { img: "/icons/recycleablePackaging.png", label: "Recyclable Packaging" },
-  { img: "/icons/petroleum.png", label: "No Petroleum" },
-  { img: "/icons/recycle.png", label: "Recyclable" },
-  { img: "/icons/biodegradable.png", label: "Biodegradable" },
-  { img: "/icons/foodSafe.png", label: "Food-Safe" },
-];
-
 const metrics = [
   { value: "180", unit: "days", label: "Decomposition cycle in soil" },
   { value: "100%", unit: "", label: "Compostable materials used" },
@@ -128,17 +119,60 @@ export function ImpactSection() {
       {/* ---------- CERTIFICATION STRIP ---------- */}
       <section className="bg-cream border-y border-black/5 px-6 py-10">
         <div className="mx-auto flex max-w-7xl flex-wrap items-end justify-center gap-14 gap-y-8 px-3 md:px-6">
-          {certifications.map(({ img, label }) => (
-            <div key={label} className="flex flex-col items-center justify-end gap-0 text-center">
-              <Image
-                src={img}
-                alt={label}
-                width={256}
-                height={256}
-                className="h-28 w-28 object-contain md:h-36 md:w-36"
-              />
-            </div>
-          ))}
+          <div className="flex flex-col items-center justify-end gap-0 text-center">
+            <Image
+              src="/icons/edible.png"
+              alt="100% Edible"
+              width={256}
+              height={256}
+              className="h-28 w-28 object-contain md:h-[140px] md:w-[140px]"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-end gap-0 text-center">
+            <Image
+              src="/icons/recycleablePackaging.png"
+              alt="Recyclable Packaging"
+              width={256}
+              height={256}
+              className="ml-5 h-28 w-28 object-contain md:h-[140px] md:w-[140px]"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-end gap-0 text-center">
+            <Image
+              src="/icons/petroleum.png"
+              alt="No Petroleum"
+              width={256}
+              height={256}
+              className="ml-5 h-28 w-28 object-contain md:h-[140px] md:w-[140px]"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-end gap-0 text-center">
+            <Image
+              src="/icons/recycle.png"
+              alt="Recyclable"
+              width={256}
+              height={256}
+              className="h-28 w-28 object-contain md:h-[144px] md:w-[156px]"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-end gap-0 text-center">
+            <Image
+              src="/icons/biodegradable.png"
+              alt="Biodegradable"
+              width={256}
+              height={256}
+              className="h-28 w-28 object-cover md:h-[144px] md:w-[125px]"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-end gap-0 text-center">
+            <Image
+              src="/icons/foodSafe.png"
+              alt="Food-Safe"
+              width={256}
+              height={256}
+              className="h-28 w-28 object-cover md:h-[144px] md:w-[126px]"
+            />
+          </div>
         </div>
       </section>
 
